df27bea0238f3d5402d6856727ffd83595572c77
k2.junior.s <k2.junior.s@8efd8c2a-510e-11de-b541-f52ad3e57354>
2009-08-22 18:32:53 +0000

    first commit

---
+++ i18n-slf4j/src/main/java/org/slf4j/i18n/impl/AnnotationLogLevelResolver.java
@@ -0,0 +1,46 @@
+package org.slf4j.i18n.impl;
+
+import java.lang.annotation.Annotation;
+import java.util.EnumMap;
+import java.util.concurrent.Callable;
+
+import org.slf4j.i18n.LogLevel;
+import org.slf4j.i18n.collections.ConcurrentEnumCache;
+import org.slf4j.i18n.helpers.ReflectionHelper;
+import org.slf4j.i18n.spi.LogBindigType;
+import org.slf4j.i18n.spi.LogLevelResolver;
+
+/**
+ * resolve log level by annotation associated with enums.
+ * 
+ * @author Takeshi Kondo
+ */
+public class AnnotationLogLevelResolver implements LogLevelResolver {
+
+	ConcurrentEnumCache<LogLevel> cache = new ConcurrentEnumCache<LogLevel>();
+
+	private <E extends Enum<E>> EnumMap<E, LogLevel> createLogLevelMap(
+			Class<E> clazz) {
+		EnumMap<E, LogLevel> map = new EnumMap<E, LogLevel>(clazz);
+		for (E e : clazz.getEnumConstants()) {
+			Annotation logLevel = ReflectionHelper.getLogLevelAnnotation(e);
+			if (logLevel != null
+					&& logLevel.annotationType().isAnnotationPresent(
+							LogBindigType.class)) {
+				LogBindigType logBindigType = logLevel.annotationType()
+						.getAnnotation(LogBindigType.class);
+				map.put(e, logBindigType.value());
+			}
+		}
+		return map;
+
+	}
+
+	public <E extends Enum<E>> LogLevel toLogLevel(final E logId) {
+		return cache.putIfAbsent(logId, new Callable<EnumMap<E, LogLevel>>() {
+			public EnumMap<E, LogLevel> call() throws Exception {
+				return createLogLevelMap(logId.getDeclaringClass());
+			}
+		});
+	}
+}
