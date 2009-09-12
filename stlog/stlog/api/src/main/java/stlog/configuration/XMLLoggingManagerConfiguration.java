b51541e3aa96c5b2d45722e646c876524eada524
k2.junior.s <k2.junior.s@8efd8c2a-510e-11de-b541-f52ad3e57354>
2009-09-12 12:54:45 +0000

    configuration api

---
+++ stlog/stlog/api/src/main/java/stlog/configuration/XMLLoggingManagerConfiguration.java
@@ -0,0 +1,20 @@
+package stlog.configuration;
+
+import java.net.URL;
+
+public class XMLLoggingManagerConfiguration extends
+		LoggingManagerConfigurationBase {
+
+	private URL url;
+
+	public XMLLoggingManagerConfiguration(URL url) {
+		super();
+		this.url = url;
+	}
+
+	@Override
+	protected void configure() {
+		new STLogXmlParser(this).parse(url);
+	}
+
+}
