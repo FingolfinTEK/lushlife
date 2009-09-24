0c943632a5a300ae833c86e19c9c72e1030e1033
k2.junior.s <k2.junior.s@8efd8c2a-510e-11de-b541-f52ad3e57354>
2009-09-24 18:59:03 +0000

    [maven-release-plugin]  copy for tag negroni-root-0.1

---
+++ tags/negroni-root-0.1/api/src/main/java/org/lushlife/negroni/MethodMissing.java
@@ -0,0 +1,38 @@
+/*
+ * Copyright 2009 the original author or authors.
+ *
+ * Licensed under the Apache License, Version 2.0 (the "License");
+ * you may not use this file except in compliance with the License.
+ * You may obtain a copy of the License at
+ *
+ *     http://www.apache.org/licenses/LICENSE-2.0
+ *
+ * Unless required by applicable law or agreed to in writing, software
+ * distributed under the License is distributed on an "AS IS" BASIS,
+ * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND,
+ * either express or implied. See the License for the specific language
+ * governing permissions and limitations under the License.
+ */
+package org.lushlife.negroni;
+
+import java.lang.annotation.ElementType;
+import java.lang.annotation.Retention;
+import java.lang.annotation.RetentionPolicy;
+import java.lang.annotation.Target;
+import java.lang.reflect.Method;
+
+/**
+ * 実装されていないメソッドを呼び出した場合にハンドリングするメソッド付加する。 第一引数は必ず{@link Method}を引数に取る。
+ * 第二引数以降は、ハンドリング対象のメソッドに合わせて実装する。
+ * 
+ * 
+ * {@link MixinMethod}と共に用いる場合には、 第一引数はオーナークラス、 第二引数は{@link Method}
+ * を引数に取る。第三引数以降は、ハンドリング対象のメソッドに合わせて実装する。
+ * 
+ * @author Takeshi Kondo
+ */
+@Target(ElementType.METHOD)
+@Retention(RetentionPolicy.RUNTIME)
+public @interface MethodMissing {
+
+}
---
+++ tags/negroni-root-0.1/api/src/main/java/org/lushlife/negroni/MixinMethod.java
@@ -0,0 +1,36 @@
+/*
+ * Copyright 2009 the original author or authors.
+ *
+ * Licensed under the Apache License, Version 2.0 (the "License");
+ * you may not use this file except in compliance with the License.
+ * You may obtain a copy of the License at
+ *
+ *     http://www.apache.org/licenses/LICENSE-2.0
+ *
+ * Unless required by applicable law or agreed to in writing, software
+ * distributed under the License is distributed on an "AS IS" BASIS,
+ * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND,
+ * either express or implied. See the License for the specific language
+ * governing permissions and limitations under the License.
+ */
+
+package org.lushlife.negroni;
+
+import java.lang.annotation.ElementType;
+import java.lang.annotation.Retention;
+import java.lang.annotation.RetentionPolicy;
+import java.lang.annotation.Target;
+
+/**
+ * mix-inされるメソッドに付加するアノテーション {@link MixinMethod}が付加されたメソッドは、 第一引数は、オーナークラスとなる。
+ * 第二引数以降は、ハンドリング対象のメソッドに合わせて実装する。
+ * 
+ * {@link MethodMissing}と併用することも可能
+ * 
+ * @author Takeshi Kondo
+ */
+@Target(ElementType.METHOD)
+@Retention(RetentionPolicy.RUNTIME)
+public @interface MixinMethod {
+
+}
