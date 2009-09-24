5d80262f6d055e5d5d9c6d8382f0522cd5436493
k2.junior.s <k2.junior.s@8efd8c2a-510e-11de-b541-f52ad3e57354>
2009-09-24 16:14:08 +0000

    add licene and auther

--- negroni-project/api2/src/main/java/org/lushlife/negroni/util/Reflections.java
+++ negroni-project/api2/src/main/java/org/lushlife/negroni/util/Reflections.java
@@ -1,3 +1,18 @@
+/*
+ * Copyright 2009 the original author or authors.
+ *
+ * Licensed under the Apache License, Version 2.0 (the "License");
+ * you may not use this file except in compliance with the License.
+ * You may obtain a copy of the License at
+ *
+ *     http://www.apache.org/licenses/LICENSE-2.0
+ *
+ * Unless required by applicable law or agreed to in writing, software
+ * distributed under the License is distributed on an "AS IS" BASIS,
+ * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND,
+ * either express or implied. See the License for the specific language
+ * governing permissions and limitations under the License.
+ */
 package org.lushlife.negroni.util;
 
 import java.lang.reflect.Array;
@@ -11,8 +26,11 @@ import java.util.Set;
 import org.lushlife.negroni.Mixined;
 import org.lushlife.negroni.conversions.Conversions;
 import org.lushlife.negroni.delegate.DelegateMethod;
-import org.lushlife.negroni.delegate.Precedence;
+import org.lushlife.negroni.delegate.DelegateMethodPrecedence;
 
+/**
+ * @author Takeshi Kondo
+ */
 public class Reflections {
 
 	static public Object invoke(Object owner, Method m, Object[] args)
@@ -64,8 +82,10 @@ public class Reflections {
 	}
 
 	static public int classCompare(DelegateMethod a, DelegateMethod b) {
-		int aValue = a.getClass().getAnnotation(Precedence.class).value();
-		int bValue = b.getClass().getAnnotation(Precedence.class).value();
+		int aValue = a.getClass().getAnnotation(DelegateMethodPrecedence.class)
+				.value();
+		int bValue = b.getClass().getAnnotation(DelegateMethodPrecedence.class)
+				.value();
 		return aValue - bValue;
 	}
 
