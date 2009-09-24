e61666ff9a222bab9b36d5d02f1b0808a7a8c0b8
k2.junior.s <k2.junior.s@8efd8c2a-510e-11de-b541-f52ad3e57354>
2009-09-24 04:00:48 +0000

    update

--- trunk/kamikaze/api/src/main/java/org/lushlife/kamikaze/resources/ResourceModule.java
+++ trunk/kamikaze/api/src/main/java/org/lushlife/kamikaze/resources/ResourceModule.java
@@ -3,13 +3,13 @@ package org.lushlife.kamikaze.resources;
 import org.lushlife.kamikaze.WebBeansBinder;
 import org.lushlife.kamikaze.WebBeansModule;
 import org.lushlife.kamikaze.spi.JaxRsServiceModle;
-import org.lushlife.kamikaze.util.jspquery.JSPQueryModule;
+import org.lushlife.kamikaze.util.UtilModule;
 
 public class ResourceModule implements WebBeansModule {
 
 	public void configure(WebBeansBinder binder) {
 		binder.installService(JaxRsServiceModle.class);
-		binder.install(new JSPQueryModule());
+		binder.install(new UtilModule());
 		binder.model(ResourceCacheImpl.class);
 	}
 }
