201d777c9b6bd6715164a55035a3aaaf591c7a3e
k2.junior.s <k2.junior.s@8efd8c2a-510e-11de-b541-f52ad3e57354>
2009-09-24 03:24:57 +0000

    rename

--- kamikaze/mvc/src/main/java/org/lushlife/kamikaze/spi/ServletEventService.java
+++
@@ -1,9 +0,0 @@
-package org.lushlife.kamikaze.spi;
-
-import javax.servlet.ServletContextListener;
-import javax.servlet.ServletRequestListener;
-
-public interface ServletEventService extends ServletContextListener,
-		ServletRequestListener {
-
-}
