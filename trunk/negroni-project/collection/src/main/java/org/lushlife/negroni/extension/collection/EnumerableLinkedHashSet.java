89427c1b687597738796f3177d49dbbc6fcf4926
k2.junior.s <k2.junior.s@8efd8c2a-510e-11de-b541-f52ad3e57354>
2009-09-24 15:47:37 +0000

    delete

--- trunk/negroni-project/collection/src/main/java/org/lushlife/negroni/extension/collection/EnumerableLinkedHashSet.java
+++
@@ -1,8 +0,0 @@
-package org.lushlife.negroni.extension.collection;
-
-import java.util.LinkedHashSet;
-
-public abstract class EnumerableLinkedHashSet<T> extends LinkedHashSet<T> implements
-		Enumerable<T> {
-
-}
