585b13a6b4d4582610238245c66dece899706299
k2.junior.s <k2.junior.s@8efd8c2a-510e-11de-b541-f52ad3e57354>
2009-09-28 11:23:39 +0000

    コメント追記

--- trunk/negroni-project/sample/src/main/java/example/MainMethodMissingMixin.java
+++ trunk/negroni-project/sample/src/main/java/example/MainMethodMissingMixin.java
@@ -6,11 +6,9 @@ import org.lushlife.negroni.Negroni;
 public class MainMethodMissingMixin {
 	public static void main(String[] args) throws InstantiationException,
 			IllegalAccessException {
-
 		Enhancer enhancer = Negroni.create();
 		Class<? extends MethodMissingMixinedSample> enhancedClass = enhancer
 				.enhace(MethodMissingMixinedSample.class);
-
 		MethodMissingMixinedSample mixinSample = enhancedClass.newInstance();
 		mixinSample.invoke();
 	}
