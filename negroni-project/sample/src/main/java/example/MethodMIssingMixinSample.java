16566d84d357f07e7f65b5e8c2e5453787f6a4e9
k2.junior.s <k2.junior.s@8efd8c2a-510e-11de-b541-f52ad3e57354>
2009-09-29 06:31:19 +0000

    sample update

--- negroni-project/sample/src/main/java/example/MethodMIssingMixinSample.java
+++ negroni-project/sample/src/main/java/example/MethodMIssingMixinSample.java
@@ -3,20 +3,17 @@ package example;
 import java.lang.reflect.Method;
 
 import org.lushlife.negroni.MissingMethod;
-import org.lushlife.negroni.MixinInterface;
+import org.lushlife.negroni.Mixin;
 import org.lushlife.negroni.MixinMethod;
 
-public class MethodMIssingMixinSample implements MixinInterface<Object> {
+public class MethodMIssingMixinSample {
+	@Mixin
+	private Object obj;
+
 	// mix-in & method-missingの実装
 	@MixinMethod
 	public void methodMissing(@MissingMethod Method method) {
 		System.out.println("mixin and method missing " + obj + " : " + method);
 	}
 
-	private Object obj;
-
-	// 実装クラスのインスタンスを取得
-	public void setMixinInstance(Object instance) {
-		this.obj = instance;
-	}
 }
--- negroni-project/sample/src/main/java/example/SampleMixinImpl.java
+++ negroni-project/sample/src/main/java/example/SampleMixinImpl.java
@@ -1,20 +1,16 @@
 package example;
 
-import org.lushlife.negroni.MixinInterface;
+import org.lushlife.negroni.Mixin;
 import org.lushlife.negroni.MixinMethod;
 
-public class SampleMixinImpl implements SampleMixin, MixinInterface<Object> {
-	Object obj;
+public class SampleMixinImpl implements SampleMixin {
+	@Mixin
+	private Object obj;
 
 	// mix-inの実装
 	@MixinMethod
 	public void mixin() {
 		System.out.println("mixin " + obj);
-	}
-
-	// 実装クラスのインスタンスを取得する
-	public void setMixinInstance(Object instance) {
-		this.obj = instance;
 	}
 
 }
