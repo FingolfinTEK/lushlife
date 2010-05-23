f3e2a28c81d7662ac2e17a1ee511e90da6ff13e1
k2.junior.s <k2.junior.s@8efd8c2a-510e-11de-b541-f52ad3e57354>
2010-05-23 08:22:50 +0000

    fix test code

--- trunk/guice-xml/src/test/java/test/SysoutInterceptor.java
+++ trunk/guice-xml/src/test/java/test/SysoutInterceptor.java
@@ -1,10 +1,16 @@
 package test;
 
+import javax.annotation.PostConstruct;
 import javax.interceptor.AroundInvoke;
 import javax.interceptor.InvocationContext;
 
 public class SysoutInterceptor {
 	String message = "system out";
+
+	@PostConstruct
+	public void post() {
+		System.out.println("post construct " + message);
+	}
 
 	@AroundInvoke
 	public Object print(InvocationContext context) throws Exception {
