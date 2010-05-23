029c239b41dd6261fd4458102f94f6297917b337
k2.junior.s <k2.junior.s@8efd8c2a-510e-11de-b541-f52ad3e57354>
2010-05-23 09:09:30 +0000

    add copyright

--- trunk/guice-xml/src/main/java/org/lushlife/guicexml/el/ThreadLocalContexts.java
+++ trunk/guice-xml/src/main/java/org/lushlife/guicexml/el/ThreadLocalContexts.java
@@ -1,10 +1,27 @@
+/*
+ * Copyright 2010 the original author or authors.
+ *
+ * Licensed under the Apache License, Version 2.0 (the "License");
+ * you may not use this file except in compliance with the License.
+ * You may obtain a copy of the License at
+ *
+ *     http://www.apache.org/licenses/LICENSE-2.0
+ *
+ * Unless required by applicable law or agreed to in writing, software
+ * distributed under the License is distributed on an "AS IS" BASIS,
+ * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND,
+ * either express or implied. See the License for the specific language
+ * governing permissions and limitations under the License.
+ */
 package org.lushlife.guicexml.el;
 
 import java.util.HashMap;
 import java.util.LinkedList;
 import java.util.Map;
 
-
+/**
+ * @author Takeshi Kondo
+ */
 public class ThreadLocalContexts {
 
 	private static ThreadLocal<LinkedList<Map<String, Object>>> context = new ThreadLocal<LinkedList<Map<String, Object>>>() {
@@ -22,7 +39,8 @@ public class ThreadLocalContexts {
 		return list.getLast();
 	}
 
-	static abstract public class Put<E extends Throwable> implements Callable<E> {
+	static abstract public class Put<E extends Throwable> implements
+			Callable<E> {
 
 		public Put(String key, Object value) throws E {
 			Map<String, Object> map = getContextMap();
